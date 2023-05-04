43858937f689818de71776a9e9384cf79c32c2ed
ivan <ohprope@gmail.com>
2023-05-04 22:04:50 +0200

    Mas ebndpoitns, usuario terminado + mascotas en proceso

--- src/routes/mascotas.routes.js
+++ src/routes/mascotas.routes.js
@@ -1,15 +1,30 @@
 import { Router } from 'express'
-import { createMascota, getMascotas, getMascota } from '../Controllers/mascotas.controller.js'
+import { createMascota, getMascotas, getMascota, deleteMascota, getMascotaStats, updateMascotaStats, updateMascota, getMascotaEnfermedades, addEnfermedad} from '../Controllers/mascotas.controller.js'
 
 const router = Router();
 
 
 
-router.post('/createMascota', createMascota)
+router.post('mascotas/createMascota', createMascota)
 
-router.get('/showMascotas', getMascotas)
+router.get('mascotas/showMascotas', getMascotas)
 
-router.get('/getMascota/:id', getMascota)
+router.get('mascotas/getMascota/:id', getMascota)
+
+router.delete('mascotas/deleteMascota/:id', deleteMascota)
+
+router.get('mascotas/getMascotaStats/:id', getMascotaStats)
+
+router.put('mascotas/updateMascota/:id', updateMascota)
+
+router.put('mascotas/updateMascotaStats/:id', updateMascotaStats)
+
+router.get('mascotas/getMascotaEnfermedades/:id', getMascotaEnfermedades)
+
+router.put('mascotas/addEnfermedad/:id', addEnfermedad)
+
+
+
 
 
 export default router;
