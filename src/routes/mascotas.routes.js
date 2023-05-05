2e82059d2b76c86da1a6e416bdc67812ba2ff0e5
ivan <ohprope@gmail.com>
2023-05-05 10:20:46 +0200

    Commit con todos los enpoints creados

--- src/routes/mascotas.routes.js
+++ src/routes/mascotas.routes.js
@@ -1,29 +1,49 @@
 import { Router } from 'express'
-import { createMascota, getMascotas, getMascota, deleteMascota, getMascotaStats, updateMascotaStats, updateMascota, getMascotaEnfermedades, addEnfermedad} from '../Controllers/mascotas.controller.js'
+import { createMascota, getMascotas, getMascota, deleteMascota, getMascotaStats, updateMascotaStats, deleteStat, updateMascota, getMascotaEnfermedades, addEnfermedad, deleteEnfermedad, getShelter, addShelter, deleteShelter, getFotos, addFoto, deleteFoto, getVideos, addVideo, deleteVideo} from '../Controllers/mascotas.controller.js'
 
 const router = Router();
 
 
 
-router.post('mascotas/createMascota', createMascota)
+router.post('/mascotas', createMascota)
 
-router.get('mascotas/showMascotas', getMascotas)
+router.get('/mascotas', getMascotas)
 
-router.get('mascotas/getMascota/:id', getMascota)
+router.get('/mascotas/getMascota/:id', getMascota)
 
-router.delete('mascotas/deleteMascota/:id', deleteMascota)
+router.delete('/mascotas/deleteMascota/:id', deleteMascota)
 
-router.get('mascotas/getMascotaStats/:id', getMascotaStats)
+router.get('/mascotas/getMascotaStats/:id', getMascotaStats)
 
-router.put('mascotas/updateMascota/:id', updateMascota)
+router.put('/mascotas/updateMascota/:id', updateMascota)
 
-router.put('mascotas/updateMascotaStats/:id', updateMascotaStats)
+router.put('/mascotas/updateMascotaStats/:id', updateMascotaStats)
 
-router.get('mascotas/getMascotaEnfermedades/:id', getMascotaEnfermedades)
+router.delete('/mascotas/deleteMascotaStat/:id', deleteStat)
 
-router.put('mascotas/addEnfermedad/:id', addEnfermedad)
+router.get('/mascotas/getMascotaEnfermedades/:id', getMascotaEnfermedades)
 
+router.put('/mascotas/addEnfermedad/:id', addEnfermedad)
 
+router.delete('/mascotas/deleteEnfermedad/:id_mascota/:id_enfermedad', deleteEnfermedad)
+
+router.get('/mascotas/shelter/:id', getShelter)
+
+router.put('/mascotas/addShelter/:id_mascota/:id_shelter', addShelter)
+
+router.delete('/mascotas/deleteShelter/:id_mascota/:id_shelter', deleteShelter)
+
+router.get('/mascotas/fotos/:id', getFotos)
+
+router.put('/mascotas/addFoto/:id_mascota/:id_foto', addFoto)
+
+router.delete('/mascotas/deleteFoto/:id_mascota/:id_foto', deleteFoto)
+
+router.get('/mascotas/getVideos/:id', getVideos)
+
+router.put('/mascotas/addVideo/:id_mascota/:id_video', addVideo)
+
+router.delete('/mascotas/deleteVideo/:id_mascota/:id_video', deleteVideo)
 
 
 
