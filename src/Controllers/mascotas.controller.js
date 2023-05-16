1131d05486fc9b53c5a04d5ace84f22cab30456b
ivan <ohprope@gmail.com>
2023-05-16 15:51:56 +0200

    ...

--- src/Controllers/mascotas.controller.js
+++ src/Controllers/mascotas.controller.js
@@ -55,7 +55,6 @@ export const getMascotaStats = async (req, res) => { // Obtener las estadística
       // Obtener las características existentes de la mascota
       const existingCharacteristics = await pool.query('SELECT id_caracteristica FROM Mascotas_Caracteristicas WHERE id_mascota = ?', [id_mascota]);
       const existingIds = existingCharacteristics.rows.map(row => row.id_caracteristica);
-      return res.json(existingIds)
       // Filtrar las características nuevas que no estén en las existentes
       const newIds = id_caracteristicas.filter(id => !existingIds.includes(id));
       
