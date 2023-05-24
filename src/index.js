384f6d3333c21f9ceb0267b80aa5d453035087e9
ivan <ohprope@gmail.com>
2023-05-24 09:57:47 +0200

    rutas funciopnan?

--- src/index.js
+++ src/index.js
@@ -9,14 +9,14 @@ import perreraRoutes from './routes/perrera.routes.js'
 
 const app = express()
 const usersYaml = YAML.load('./src/Yaml/api.yaml')
-app.use('/', swaggerUi.serve, swaggerUi.setup(usersYaml))
+app.use('/hundopt/api', swaggerUi.serve, swaggerUi.setup(usersYaml))
 
 app.use(express.json())
 
-app.use(mascotasRoutes)
-app.use(appRoutes)
-app.use(userRoutes)
-app.use(perreraRoutes)
+app.use('/hundopt/api',mascotasRoutes)
+app.use('/hundopt/api',appRoutes)
+app.use('/hundopt/api',userRoutes)
+app.use('/hundopt/api',perreraRoutes)
 app.use((req, res, next) => {
     res.status(404).json({ message: 'Not found' })
 })
